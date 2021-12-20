6e2af5ea72dc0e36a660e247378e6353f0c3fb74
JBKIM <drv98@naver.com>
2021-12-20 10:13:26 +0900

    6장 액션바와 프래그먼트

--- actionbar_fragment/src/main/java/com/example/actionbar_fragment/MainActivity.java
+++ actionbar_fragment/src/main/java/com/example/actionbar_fragment/MainActivity.java
@@ -1,14 +1,107 @@
 package com.example.actionbar_fragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-    @Override
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener{
+
+    ActionBar.Tab tabSong, tabArtist, tabAlbum;
+    MyTabFragment myFrags[] = new MyTabFragment[3];
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActionBar bar = getSupportActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        tabSong = bar.newTab();
+        tabSong.setText("음악별");
+        tabSong.setTabListener(this);
+        bar.addTab(tabSong);
+
+        tabArtist = bar.newTab();
+        tabArtist.setText("가수별");
+        tabArtist.setTabListener(this);
+        bar.addTab(tabArtist);
+
+        tabAlbum = bar.newTab();
+        tabAlbum.setText("앨범별");
+        tabAlbum.setTabListener(this);
+        bar.addTab(tabAlbum);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        MyTabFragment myTabFrag = null;
+        if (myFrags[tab.getPosition()] == null) {
+
+            myTabFrag = new MyTabFragment();
+            Bundle data = new Bundle();
+            data.putString("tabName", tab.getText().toString());
+
+            myTabFrag.setArguments(data);
+            myFrags[tab.getPosition()] = myTabFrag;
+        } else
+            myTabFrag = myFrags[tab.getPosition()];
+
+
+        ft.replace(android.R.id.content, myTabFrag);
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    //2.내부 클래스
+    public static class MyTabFragment extends Fragment
+    {
+        String tabName;
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Bundle data = getArguments();
+            tabName = data.getString("tabName");
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+        {
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                                                                            LinearLayout.LayoutParams.MATCH_PARENT,
+                                                                            LinearLayout.LayoutParams.MATCH_PARENT);
+
+            LinearLayout baseLayout = new LinearLayout(super.getActivity());
+            baseLayout.setOrientation(LinearLayout.VERTICAL);
+            baseLayout.setLayoutParams(params);
+            if (tabName == "음악별")
+                baseLayout.setBackgroundColor(Color.RED);
+            if (tabName == "가수별")
+                baseLayout.setBackgroundColor(Color.GREEN);
+            if (tabName == "앨범별")
+                baseLayout.setBackgroundColor(Color.BLUE);
+
+            return baseLayout;
+        }
     }
 }
+
+//1.대등한 클래스
